d9e0470365eac5a7e33412eac9f30b619e652752
AykutEngur <155203829+AykutEngur@users.noreply.github.com>
2024-10-05 14:24:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ if "username" not in st.session_state:
 def display_footer():
     st.markdown(
         """
-        <div style='position: fixed; top: 10px; right: 10px; color: #4CAF50; font-size: 16px; z-index: 1000;'>
+        <div style='position: fixed; top: 20px; right: 20px; color: #4CAF50; font-size: 16px;'>
             Developed by <strong>Aykut Engür</strong>
         </div>
         """,
