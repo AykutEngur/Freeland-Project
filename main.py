1d54041e16faa6d3fa56638440f076741bb61d9a
AykutEngur <155203829+AykutEngur@users.noreply.github.com>
2024-10-05 15:59:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -169,8 +169,8 @@ if "username" not in st.session_state:
 def display_footer():
     st.markdown(
         """
-        <div style='color: gray;'>
-            Developed by Aykut Engür
+        <div style='position: fixed; top: 30px; right: 10px; color: gray; font-size: 25px;'>
+            Developed by <strong>Aykut Engür</strong>
         </div>
         """,
         unsafe_allow_html=True
